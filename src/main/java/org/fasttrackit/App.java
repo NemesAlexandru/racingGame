0d62d41b95164e5fcbc8ffc1907d1c86f77722ab
NemesAlexandru <alexandrunemes1@gmail.com>
2019-09-02 21:19:10 +0300

    studied modulo, ++ and --

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,5 +1,7 @@
 package org.fasttrackit;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
@@ -48,6 +50,14 @@ public class App {
 
         car = car2;
 
+        System.out.println("Modulo Examples");
+        System.out.println(4 % 2);
+        System.out.println(4 % 3);
+
+        System.out.println("Please Enter Some Text");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        System.out.println("Input from User " + text);
 
     }
 }
